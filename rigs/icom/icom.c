41405c394f26e760a4bbdd5611a28c5a2db7035a
Wouter van Gulik <pa3wle@gmail.com>
2021-09-24 22:14:47 +0200

    Icom: add sanity check on icom_get_ts and icom_set_ts: list cannot be NULL. Add debug message when tuningstep is not in the list.

--- rigs/icom/icom.c
+++ rigs/icom/icom.c
@@ -6301,6 +6301,14 @@ int icom_set_ts(RIG *rig, vfo_t vfo, shortfreq_t ts)
     rig_debug(RIG_DEBUG_VERBOSE, "%s called\n", __func__);
     priv_caps = (const struct icom_priv_caps *) rig->caps->priv;
 
+    /* This should never happen, but it is easy to misconfigure so check
+     * instead of plain crashing. */
+    if (!priv_caps->ts_sc_list)
+    {
+        rig_debug(RIG_DEBUG_ERR, "%s: Tuningstep list is NULL!\n", __func__);
+        RETURNFUNC(-RIG_EINTERNAL);
+    }
+
     for (i = 0; priv_caps->ts_sc_list[i].ts > 0; i++)
     {
         if (priv_caps->ts_sc_list[i].ts == ts)
@@ -6312,6 +6320,8 @@ int icom_set_ts(RIG *rig, vfo_t vfo, shortfreq_t ts)
 
     if (ts_sc == 0)
     {
+        rig_debug(RIG_DEBUG_ERR, "%s: %ld not in tuningstep list!\n",
+                __func__, ts);
         RETURNFUNC(-RIG_EINVAL);   /* not found, unsupported */
     }
 
@@ -6354,6 +6364,14 @@ int icom_get_ts(RIG *rig, vfo_t vfo, shortfreq_t *ts)
     rig_debug(RIG_DEBUG_VERBOSE, "%s called\n", __func__);
     priv_caps = (const struct icom_priv_caps *) rig->caps->priv;
 
+    /* This should never happen, but it is easy to misconfigure so check
+     * instead of plain crashing. */
+    if (!priv_caps->ts_sc_list)
+    {
+        rig_debug(RIG_DEBUG_ERR, "%s: Tuningstep list is NULL!\n", __func__);
+        RETURNFUNC(-RIG_EINTERNAL);
+    }
+
     retval = icom_transaction(rig, C_SET_TS, -1, NULL, 0, tsbuf, &ts_len);
 
     if (retval != RIG_OK)
@@ -6384,6 +6402,8 @@ int icom_get_ts(RIG *rig, vfo_t vfo, shortfreq_t *ts)
 
     if (!found)
     {
+        rig_debug(RIG_DEBUG_ERR, "%s: %d not in tuningstep list!\n",
+                __func__, tsbuf[1]);
         RETURNFUNC(-RIG_EPROTO);   /* not found, unsupported */
     }
 
